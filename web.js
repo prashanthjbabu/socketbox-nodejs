e2953a8421e4cb7e9e4b94dadcf8dea7c1954465
prashanth <prashanthjbabu@gmail.com>
2013-07-02 21:06:09 +0530

    removing excess appuse

--- web.js
+++ web.js
@@ -13,9 +13,9 @@ app.configure(function() {
 	/*app.set('views', __dirname + '/views');
 	app.set('view engine', 'ejs');
 	app.use(express.favicon());
-	app.use(express.logger('dev'));*/
+	app.use(express.logger('dev'));
 	app.use(express.bodyParser());
-	app.use(express.methodOverride());
+	app.use(express.methodOverride());*/
 	app.use(app.router);
 	app.use(express.static(__dirname + '/public'));
 });
